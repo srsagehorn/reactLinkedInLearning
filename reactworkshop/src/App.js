794a2902c7d2a286d1f312152ff5b4e795e681d9
Shannon Sagehorn <srsagehorn@gmail.com>
2021-01-20 17:00:03 -0600

    units 1-3

--- reactworkshop/src/App.js
+++ reactworkshop/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+function SecretComponent() {
+  return(
+    <h1>Secret information for authorized users only</h1>
+  )
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function RegularComponent() {
+  return(
+    <h1>Everyone can see this component</h1>
+  )
+}
+
+function App(props) {
+  if (props.authorized) {
+    return(<SecretComponent/>)
+  }
+  else {
+    return(<RegularComponent />)
+  }
 }
 
 export default App;
